c0303a77b4c07a9f33c3ea45bc6834caa3914457
Matt Hagen <anonz3000@gmail.com>
2022-04-09 16:04:30 -0400

    Adding deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-cp ~/hagenhaus-com/assets/favicon.png ~/htdocs/assets/favicon.png
-cp ~/hagenhaus-com/assets/logo-transparent.png ~/htdocs/assets/logo-transparent.png
-cp ~/hagenhaus-com/assets/styles.min.css ~/htdocs/assets/styles.min.css
-cp ~/hagenhaus-com/assets/scripts.min.js ~/htdocs/assets/scripts.min.js
-cp ~/hagenhaus-com/index.html ~/htdocs/index.html
-cp ~/hagenhaus-com/en/index.html ~/htdocs/en/index.html
-cp ~/hagenhaus-com/en/home/config.json ~/htdocs/en/home/config.json
-cp ~/hagenhaus-com/en/home/index.html ~/htdocs/en/home/index.html
-cp ~/hagenhaus-com/en/home/otp.html ~/htdocs/en/home/otp.html
-cp ~/hagenhaus-com/en/home/page.html ~/htdocs/en/home/page.html
+install -D -m 644 ~/hagenhaus-com/assets/favicon.png ~/htdocs/assets/favicon.png
+install -D -m 644 ~/hagenhaus-com/assets/logo-transparent.png ~/htdocs/assets/logo-transparent.png
+install -D -m 644 ~/hagenhaus-com/assets/styles.min.css ~/htdocs/assets/styles.min.css
+install -D -m 644 ~/hagenhaus-com/assets/scripts.min.js ~/htdocs/assets/scripts.min.js
+install -D -m 644 ~/hagenhaus-com/index.html ~/htdocs/index.html
+install -D -m 644 ~/hagenhaus-com/en/index.html ~/htdocs/en/index.html
+install -D -m 644 ~/hagenhaus-com/en/home/config.json ~/htdocs/en/home/config.json
+install -D -m 644 ~/hagenhaus-com/en/home/index.html ~/htdocs/en/home/index.html
+install -D -m 644 ~/hagenhaus-com/en/home/otp.html ~/htdocs/en/home/otp.html
+install -D -m 644 ~/hagenhaus-com/en/home/page.html ~/htdocs/en/home/page.html
